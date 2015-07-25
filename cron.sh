bec2c048fe33cb7cb06a91b9500c9870329d4009
Ward Cunningham <ward@c2.com>
2015-07-25 01:11:41 -0700

    publish activity in cron

--- cron.sh
+++ cron.sh
@@ -8,6 +8,7 @@ ruby scrape.rb > logs/`date -u +%a-%H00`
 find sites -name words.txt -newer words.txt | \
 	cut -d / -f 2 | \
 	sort | uniq > activity/`date -u +%a-%H00`
+ruby activity.rb
 
 
 ruby rollup.rb
