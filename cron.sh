58e62189899b10bd69bc1edfee46d5eb87e9c46c
Ward Cunningham <ward@c2.com>
2015-08-04 04:44:19 -0700

    reorder so new sites.txt is available

--- cron.sh
+++ cron.sh
@@ -11,9 +11,8 @@ find sites -name words.txt -newer words.txt | \
 	cut -d / -f 2 | \
 	sort | uniq > activity/`date -u +%a-%H00`
 
-ruby found.rb
-ruby activity.rb
-
-
 ruby rollup.rb
 tar czf public/sites.tgz sites *.txt
+
+ruby found.rb
+ruby activity.rb
