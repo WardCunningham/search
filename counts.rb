eeb213f7700a3d6c5ac02575aea4d220752190d9
Ward Cunningham <ward@c2.com>
2015-09-05 15:23:20 -0700

    save counts for longitudinal analysis

--- counts.rb
+++ counts.rb
@@ -1,6 +1,8 @@
 # report sites and pages scanned from most recent log
 # usage: ruby counts.rb
 
+require 'json'
+
 recent = `ls -t logs | head -n 1`.chomp
 log = File.read "logs/#{recent}"
 
@@ -9,5 +11,9 @@ log.scan(/, (\d+) pages, /) do |count|
 	sites += 1
 	pages += count[0].to_i
 end
+
+counts = {scan: {sites: sites, pages: pages}, index: {}}
+`wc -l *.txt`.scan(/(\d+) (\w+)\.txt/) {|v,k| counts[:index][k] = v.to_i}
+File.open('counts.txt','a') {|file| file.puts counts.to_json}
 
 puts "We scanned #{sites} sites hosting #{pages} pages."
