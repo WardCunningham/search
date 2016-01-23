b88814c72a4bb7e31bd20706f3f0017476b91e8a
Ward Cunningham <ward@c2.com>
2016-01-23 13:41:23 -0800

    build neo4j db on remote server

---
+++ neo-build.sh
@@ -0,0 +1,18 @@
+# build neo4j db on remote server
+# usage: sh neo-build.sh
+
+server='root@bay.wiki.org'
+path='/var/lib/neo4j/data'
+
+scp public/{nodes,rels}.csv $server:$path
+
+build="
+date
+cd $path
+neo4j-import --into new.db --nodes nodes.csv --relationships rels.csv
+chmod -R a+w new.db
+mv wiki.db old.db
+mv new.db wiki.db
+service neo4j-service restart
+"
+ssh $server "$build" >public/neo-build.log
