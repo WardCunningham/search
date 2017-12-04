8718bca3a0b88c172939ab72ff4c1ed2501fb261
Ward Cunningham <ward@c2.com>
2017-12-03 16:24:38 -0800

    visible federation organized by ip address

--- roster.rb
+++ roster.rb
@@ -1,74 +1,45 @@
-require 'pp'
-@tree = {}
+# construct roster for whole federation from sites scraped
+# usage: roster = `ruby roster.rb`
 
-def insert domain
-	branch = @tree
-	domain.split('.').reverse().each do |name|
-		branch[name] ||= {}
-		branch = branch[name]
-	end
-	branch[''] = []
+@hosts = {}
+
+def tally site, addr
+  if m = site.match(/([^.]+\.[^.]+)$/)
+    @hosts[addr] ||= {}
+    @hosts[addr][m[1]] ||= []
+    @hosts[addr][m[1]] << site
+  end
+end
+
+def dns domain
+  return if domain =~ /^www\./
+  dns = `host #{domain}`
+  if m = dns.match(/^(.+?) is an alias for (.+?).\n\2 has address ([\d.]+)$/)
+    tally m[1], m[3]
+  elsif m = dns.match(/^(.+?) has address ([\d.]+)$/)
+    tally m[1], m[2]
+  end
 end
 
 def input
-	list = `cd sites; ls | while read i; do echo \`ls $i/pages | wc -l\` $i; done 2>/dev/null`
-	list.split("\n").each do |line|
-		count, domain = line.split ' '
-		insert domain if count.to_i >= 10
-	end
+  list = `cd sites; ls | while read i; do echo \`ls $i/pages | wc -l\` $i; done 2>/dev/null`
+  list.split("\n").each do |line|
+    count, domain = line.split ' '
+    dns domain if count.to_i >= 5
+  end
 end
 
-def depth node, n=0
-	all = [n]
-	node.each do |key, value|
-		all << depth(value, n+1)
-	end
-	all.max
-end
-
-def total node
-	sum = 1
-	node.each do |key, value|
-		sum += total(value)
-	end
-	sum
-end
-
-def group? node
-	depth(node) == 2 or total(node) < 140
-end
-
-def leaf? node
-	node.empty?
-end
-
-def cite path
-	puts path[0..-2].reverse().join('.')
-end
-
-def look path, node
-	# puts "-------- #{path.reverse().join('.')} #{depth node} #{total node}" 
-	if group? node
-		puts "\n#{path.reverse().join(' ')}\n\n" 
-		cite(path) if leaf?(node)
-		node.sort().each do |name, more|
-			show path+[name], more
-		end
-	else
-		cite(path) if leaf?(node)
-		node.sort().each do |name, more|
-			look path+[name], more
-		end
-	end
-end
-
-def show path, node
-	cite(path) if leaf?(node)
-	node.sort().each do |name, more|
-		show path+[name], more
-	end
+def roster
+  @hosts.each do |addr, domains|
+    puts addr.gsub(/\./,'-')
+    puts
+    domains.each do |domain, sites|
+      puts sites.join("\n")
+      puts
+    end
+    puts
+  end
 end
 
 input
-look [], @tree
-# pp @tree
+roster
