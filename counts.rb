6d1f5019d8875819d84f97df19f7aed7f45bf227
Ward Cunningham <ward@c2.com>
2020-12-27 11:48:45 -0800

    experimental webhooks

--- counts.rb
+++ counts.rb
@@ -17,3 +17,8 @@ counts = {date: Time.now.to_i, scan: {sites: sites, pages: pages}, index: {}}
 File.open('counts.txt','a') {|file| file.puts counts.to_json}
 
 puts "We scanned #{sites} sites hosting #{pages} pages."
+
+puts "http://3d.local:6789/scrape"
+puts `curl -XPOST -H 'Auth:3546736' -H 'Content-type:text/json' http://3d.local:6789/scrape -d '#{counts.to_json}'`
+puts "https://rest.livecode.world/scrape"
+puts `curl -XPOST -H 'Auth:3546736' -H 'Content-type:text/json' https://rest.livecode.world/scrape -d '#{counts.to_json}'`
