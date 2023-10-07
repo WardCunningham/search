a42ab1fb09b5fa3073465f499401d45c144afd20
Ward Cunningham <ward@c2.com>
2023-10-06 21:07:42 -0700

    build and write graph nodes and rels

--- doxref.rb
+++ doxref.rb
@@ -1,5 +1,7 @@
 # test xref to graph conversion
 
+require 'json'
+
 # cron.sh 45 Index ► Shell:cron ► runs Ruby:slug-web ► write Public:slug-web.json
 # scrape.rb 11 Index ► Sites:dir ► read Ruby:scrape
 
@@ -26,25 +28,42 @@ puts lines
 # {:file=>"scrape.rb", :num=>"11", :sys=>"Index", :code=>["Sites:dir", "read Ruby:scrape"]}
 
 
-def node(graph,tuple)
-  return tuple
+def node(sys,graph,tuple)
+  type,name = tuple.split(/:/)
+  nid = graph[:nodes].find_index{|node| node[:type]==type && node[:props][:name]==name}
+  if (!!nid)
+    return nid
+  else
+    want = {type:type,in:[],out:[],props:{name:name}}
+    graph[:nodes].push(want)
+    return graph[:nodes].length-1
+  end
 end
 
 def rel(graph,type,here,there)
-  puts "#{type} from #{here} to #{there}"
+  puts "#{type} from #{tuple(graph,here)} to #{tuple(graph,there) }"
+  graph[:rels].push({type:type,from:here,to:there,props:{}})
+  rid = graph[:rels].length-1
+  graph[:nodes][here][:out].push(rid)
+  graph[:nodes][there][:in].push(rid)
+end
+
+def tuple(graph,nid)
+  node=graph[:nodes][nid]
+  "#{node[:type]}:#{node[:props][:name]}"
 end
 
 graphs = Hash.new { {nodes:[],rels:[]} }
 lines.each {|line|
-  puts "#{line[:file]} #{line[:num]}"
+  puts "\n#{line[:sys]} in #{line[:file]} #{line[:num]}"
   graph = graphs[line[:sys]]
   here = nil
   line[:codes].each { |code|
     first, rest = code.split(/ /)
     if (first.include?(':'))
-      here = node(graph,first)
+      here = node(line[:sys],graph,first)
     else
-      there = node(graph,rest)
+      there = node(line[:sys],graph,rest)
       rel(graph,first,here,there)
     end
   }
@@ -57,5 +76,8 @@ lines.each {|line|
 # scrape.rb 11
 # read from Sites:dir to Ruby:scrape
 
-
-puts graphs
+graphs.each {|sys,graph|
+  File.open("Xref#{sys}.graph.json", 'w') { |file|
+    file.write(graph.to_json)
+  }
+}
