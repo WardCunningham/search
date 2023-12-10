faf96ca4dbb5624fc588c731d26115f85f47c850
Ward Cunningham <ward@c2.com>
2023-12-10 14:56:17 -0800

    state or country as state

--- sqlite/select-all-cities.sh
+++ sqlite/select-all-cities.sh
@@ -1,8 +1,8 @@
 # Select fields for cities of interest
-# Usage: cd sqlite; sh select-all-cites.sh > geonames.json
+# Usage: cd sqlite; sh select-all-cities.sh > geonames-all.json
 
 cat geonames-all-cities-with-a-population-1000.json |\
  jq '[
    .[] |
-   [{name:.ascii_name, state:.admin1_code, country:.cou_name_en}, .population, .coordinates]
+   [{name:.ascii_name, state:(if .cou_name_en=="United States" then .admin1_code+" USA" else .cou_name_en end )}, .population, .coordinates]
  ]'
